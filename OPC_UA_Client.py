6d2cfc185d9dba7eedfb3bbab6d8b2efee75a054
Keypos <jonas.morsch@t-online.de>
2022-05-02 20:29:54 +0200

    Client läuft

--- OPC_UA_Client.py
+++ OPC_UA_Client.py
@@ -6,11 +6,25 @@ url = "opc.tcp://127.0.0.1:4840"
 client = Client(url)
 
 client.connect()
+
 print("Client Connected")
 
-while True:
-           Temp = client.get_node("ns=2;i=2")
-           Temperature = Temp.get_value()
-           print(Temperature)
+try:
+    while True:
+        Dat = client.get_node("ns=2;i=4")
+        Temp = client.get_node("ns=2;i=2")
+        Pres = client.get_node("ns=2;i=3")
 
+        Pressure = Pres.get_value()
+        Temperature = Temp.get_value()
+        Date = Dat.get_value()
+
+        print("Date: " + str(Date))
+        print("Temperature: " + str(Temperature))
+        print("Pressure: " + str(Pressure))
+
+        time.sleep(0.8)
+except KeyboardInterrupt:
+    pass
 print("Client closed")
+client.disconnect()
