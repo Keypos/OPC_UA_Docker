732892b4789668dddcf134b59b4e92ea3446ae98
Jonas Morsch <jonas.morsch@t-online.de>
2022-05-02 17:27:17 +0200

    OPC works fine

---
+++ OPC_UA_Client.py
@@ -0,0 +1,16 @@
+from opcua import Client
+import time
+
+url = "opc.tcp://127.0.0.1:4840"
+
+client = Client(url)
+
+client.connect()
+print("Client Connected")
+
+while True:
+           Temp = client.get_node("ns=2;i=2")
+           Temperature = Temp.get_value()
+           print(Temperature)
+
+print("Client closed")
